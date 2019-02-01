fd5316a172b5710b21afb0e2d40c71f63d3d657b
Unknown <helpu1409@gmail.com>
2019-01-31 23:06:52 -0500

    Actualizando el archivo de topologia test

--- enero/30/topo-test.py
+++ enero/30/topo-test.py
@@ -1,42 +1,73 @@
 #!/usr/bin/python
 
 """
-This example shows how to create a network and run multiple tests.
-For a more complicated test example, see udpbwtest.py.
+Simple example of sending output to multiple files and
+monitoring them
 """
 
-from mininet.cli import CLI
-from mininet.node import OVSKernelSwitch
-from mininet.topolib import TreeTopo
-from mininet.util import custom, pmonitor
-from mininet.log import setLogLevel, info
-from mininet.node import OVSSwitch, Controller, RemoteController
-from mininet.cli import CLI
+
+from mininet.topo import SingleSwitchTopo
 from mininet.net import Mininet
 from mininet.log import info, setLogLevel
+from mininet.node import OVSSwitch, Controller, RemoteController
+from time import time
+from select import poll, POLLIN
+from subprocess import Popen, PIPE
 from mininet.link import TCLink
-
-import time
-import os
-setLogLevel( 'info' )
 
 
 # sudo ryu-manager --verbose simple_switch_13.py ofctl_rest.py
-
-
-def getPing( h1, h2, veces ):
-    popens = {}
-    popens[h1] = h1.popen("ping -c %s %s" %(veces, h2.IP()))
-    # Monitor them and print output
-    #host, line = pmonitor(popens)
-    #info("<%s>: %s" % (host.name, line))
-    for host, line in pmonitor(popens):
-        if host:
-            info("<%s>: %s" % (host.name, line))
-    # Done
-
-
 setLogLevel('info')
+
+def monitorFiles(hSrc, outfile, seconds, timeoutms ):
+    "Monitor set of files and return [(host, line)...]"
+    devnull = open( '/dev/null', 'w' )
+    tails, fdToFile, fdToHost = {}, {}, {}
+    tail = Popen( [ 'tail', '-f', outfile ],
+                      stdout=PIPE, stderr=devnull )
+    fd = tail.stdout.fileno()
+    fdToFile = tail.stdout
+    fdToHost = hSrc
+    # Prepare to poll output files
+    readable = poll()
+    readable.register( tail.stdout.fileno(), POLLIN )
+    # Run until a set number of seconds have elapsed
+    endTime = time() + seconds
+    while time() < endTime:
+        fdlist = readable.poll(timeoutms)
+        if fdlist:
+            for fd, _flags in fdlist:
+                f = fdToFile
+                # Wait for a line of output
+                line = f.readline().strip()
+                # yield host , decode( line )
+                yield hSrc, line
+        else:
+            # If we timed out, return nothing
+            yield None, ''
+    for t in tails.values():
+        t.terminate()
+    devnull.close()  # Not really necessary
+
+
+def pingTest( hSrc, hDst, seconds=3 ):
+    outfile = 'ping.out'
+    errfile = 'ping.err'
+    hSrc.cmd('echo >', outfile)
+    hSrc.cmd('echo >', errfile)
+    # Start pings
+
+    hSrc.cmdPrint('ping', hDst.IP(),
+               '>', outfile,
+               '2>', errfile,
+               '&')
+    info( "Monitoring output for", seconds, "seconds\n" )
+    for h, line in monitorFiles( hSrc,outfile, seconds, timeoutms=500 ):
+        if h:
+            info( '%s: %s\n' % ( h.name, line ) )
+    hSrc.cmd('kill %ping')
+
+
 
 info('*** Create the controller \n')
 
@@ -68,11 +99,15 @@ net.build()
 info('*** Starting network\n')
 net.start()
 
-getPing( h1, h2, 5)
+pingTest(h1, h2, 5)
 #info('*** Running CLI\n')
 #CLI(net)
 
 info('*** Stopping network')
 net.stop()
+
+
+if __name__ == '__main__':
+    setLogLevel('info')
 
 
