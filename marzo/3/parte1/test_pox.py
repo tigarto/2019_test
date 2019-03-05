227e7f3245aa62c7a1ea2ac59baafc148bd55c7c
Unknown <helpu1409@gmail.com>
2019-03-05 18:14:00 -0500

    actualizando ll de marzo 3

--- marzo/3/parte1/test_pox.py
+++ marzo/3/parte1/test_pox.py
@@ -1,28 +1,33 @@
 #!/usr/bin/python
 
-from mininet.log import setLogLevel
+from mininet.log import setLogLevel, info
 from mininet.net import Mininet
 from mininet.topolib import TreeTopo
 from mininet.cli import CLI
+from time import time, sleep
 
 # We assume you are in the same directory as pox.py
 # or that it is loadable via PYTHONPATH
 from controllers import RYU,POX
-import os
-
-
 
 setLogLevel( 'info' )
 
+
+'''
+# ENSAYO CON RYU --> ok
+
+net = Mininet( topo=TreeTopo( depth=2, fanout=2 ),
+               controller=RYU )
+'''
 
 
 # ENSAYO CON POX --> ok
 net = Mininet( topo=TreeTopo( depth=2, fanout=2 ),
                controller=POX )
-
-if __name__ == "__main__":
-  net.start()
-  net.pingAll() 
-  net.iperf()
-  net.stop()
-
+sleep(3)
+net.start()
+sleep(2)
+c0 = net.getNodeByName('c0')
+info(c0.port,'\n')
+CLI( net )
+net.stop()
--- marzo/3/parte1/test_ryu.py
+++ marzo/3/parte1/test_ryu.py
@@ -8,21 +8,22 @@ from mininet.cli import CLI
 # We assume you are in the same directory as pox.py
 # or that it is loadable via PYTHONPATH
 from controllers import RYU,POX
-import os
-
-
 
 setLogLevel( 'info' )
 
 
+'''
+# ENSAYO CON RYU --> ok
+'''
+net = Mininet( topo=TreeTopo( depth=2, fanout=2 ),
+               controller=RYU )
 
+
+'''
 # ENSAYO CON POX --> ok
 net = Mininet( topo=TreeTopo( depth=2, fanout=2 ),
                controller=POX )
-
-if __name__ == "__main__":
-  net.start()
-  net.pingAll() 
-  net.iperf()
-  net.stop()
-
+'''
+net.start()
+CLI( net )
+net.stop()
