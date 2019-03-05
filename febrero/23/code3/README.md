4956a05faad4073d15e9549fabceebcbfac0f2c8
Unknown <helpu1409@gmail.com>
2019-03-05 00:39:37 -0500

    lo ultimo en febrero 23

---
+++ febrero/23/code3/README.md
@@ -0,0 +1,21 @@
+Como el pox no tiene soporte para OF 1.3 (http://sdnhub.org/tutorials/pox/) se uso el siguiente: https://github.com/vsulak/pox-1.3.
+
+
+```bash
+cd ~
+git clone https://github.com/vsulak/pox-1.3.git
+```
+
+Para el caso se tiene entonces que:
+
+
+
+Veamos con ONOS:
+
+https://github.com/opennetworkinglab/onos
+
+
+/opt/onos/bin/onos-service start
+
+https://www.sanog.org/resources/sanog29/SANOG29-Tutorial_ONOS-Imran.pdf
+https://guide.opencord.org/cord-6.1/onos-service/
--- febrero/23/code3/test.py
+++ febrero/23/code3/test.py
@@ -7,13 +7,23 @@ from mininet.cli import CLI
 
 # We assume you are in the same directory as pox.py
 # or that it is loadable via PYTHONPATH
-from ryu import RYU
+from controllers import RYU,POX
 
 setLogLevel( 'info' )
 
+
+'''
+# ENSAYO CON RYU --> ok
+'''
 net = Mininet( topo=TreeTopo( depth=2, fanout=2 ),
                controller=RYU )
 
+
+'''
+# ENSAYO CON POX --> ok
+net = Mininet( topo=TreeTopo( depth=2, fanout=2 ),
+               controller=POX )
+'''
 net.start()
 CLI( net )
 net.stop()
