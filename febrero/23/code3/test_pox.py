331596ab17a7b432f18bcbac1171932ce2a398e7
Unknown <helpu1409@gmail.com>
2019-03-05 00:48:50 -0500

    actualizando febrero 23

---
+++ febrero/23/code3/test_pox.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+from mininet.log import setLogLevel
+from mininet.net import Mininet
+from mininet.topolib import TreeTopo
+from mininet.cli import CLI
+
+# We assume you are in the same directory as pox.py
+# or that it is loadable via PYTHONPATH
+from controllers import RYU,POX
+
+setLogLevel( 'info' )
+
+
+'''
+# ENSAYO CON RYU --> ok
+
+net = Mininet( topo=TreeTopo( depth=2, fanout=2 ),
+               controller=RYU )
+'''
+
+
+# ENSAYO CON POX --> ok
+net = Mininet( topo=TreeTopo( depth=2, fanout=2 ),
+               controller=POX )
+
+net.start()
+CLI( net )
+net.stop()
