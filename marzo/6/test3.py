b67651b41d498d937a416a6dac7315c20a0334a4
Unknown <helpu1409@gmail.com>
2019-03-27 00:23:03 -0500

    motilando archivo

--- marzo/6/test3.py
+++ marzo/6/test3.py
@@ -2,12 +2,6 @@ import psutil
 import subprocess
 from netstat import netstat
 
-"""
-print "hola"
-for proc in psutil.process_iter(attrs=['pid', 'name']):
-    if "mininet" in proc.info['name']:
-        print proc.info['name']
-"""
 
 """
 sudo ryu-manager --verbose simple_switch_13.py ofctl_rest.py
@@ -17,7 +11,6 @@ sudo mn --topo=single,3 --mac --switch=ovsk,protocols=OpenFlow13 --controller=re
 """
 
 pl = subprocess.Popen(['ps', '-ax','-o','pid,cmd'], stdout=subprocess.PIPE).communicate()[0]
-#print pl
 lines = pl.split('\n')
 mininet_lines = []
 for l in lines:
@@ -30,23 +23,18 @@ dicProcesos = {}
 for p in mininet_lines:
     l = p.split()
     if ("h" in l[-1]) or ("s" in l[-1]):        
-        # print l
         # Agregando hosts y el swith
         node = l[-1]
         dicProcesos[node[node.find(':')+1:]] = int(l[0])
 
+# Agregando el controlador
 port = 6653
 for conn in netstat():
     # print conn
     if str(6653) in conn[2] and ('0.0.0.0' in conn[2]):
         dicProcesos['c0'] = int(conn[5]) 
 
-# print dicProcesos
-
-# informacion de los procesos  
-# https://thispointer.com/python-get-list-of-all-running-processes-and-sort-by-highest-memory-usage/
 procesos = []
-
 for k in dicProcesos:
     p = psutil.Process(dicProcesos[k])
     dic_attr = p.as_dict(attrs=['pid', 'name', 'cpu_percent'])
