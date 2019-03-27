3e72fc150bf7e2778f74908c675c8f2c81085d85
Unknown <helpu1409@gmail.com>
2019-03-26 19:33:13 -0500

    actualizando archivo para la generacion de experimentos

--- marzo/9/fw/alpha/disenoExperimental.py
+++ marzo/9/fw/alpha/disenoExperimental.py
@@ -1,20 +1,3 @@
-import os
-from mininet.net import Mininet
-from mininet.log import info, setLogLevel
-from mininet.node import OVSSwitch, Controller, RemoteController
-from mininet.link import TCLink
-from mininet.topo import Topo
-import subprocess
-from time import time, sleep
-import psutil
-from mininet.cli import CLI
-from subprocess import Popen, PIPE, STDOUT
-from select import poll, POLLIN
-from trafico import TraficoAtaque, TraficoNormal
-from unidadExperimental import UnidadExperimental
-from topologia import TopologiaPOX, TopologiaRyu, TopologiaTest
-from controlador import RYU, POX
-from experimento import Experimento
 import dexpy.factorial
 import dexpy.power
 import dexpy.alias
@@ -105,7 +88,6 @@ def obtenerTratamientos(niveles):
     niveles_factor = {}
     for k in niveles:
         niveles_factor[k] = len(niveles[k])
-    # print(niveles_factor.keys(),niveles_factor.values())
     tratamientos = fullfact(niveles_factor.values())    
     return tratamientos
 
@@ -114,31 +96,19 @@ def codificarTratamientos(niveles):
     tr = obtenerTratamientos(niveles)
     tr = tr.astype(int)
     tr_cod = tr.astype(str)
-    #print(tr_cod)
     num_rows = tr.shape[0]
     i = 0
     index_fac = 0
     index_row = 0
     for e in np.nditer(tr, order='F'):        
         if i < num_rows:
-            #e = factores[index_fac][int(e)]
-            #print(e)
-            
-            #ii = int(e)
-            #print(type(i))
-             
-            #e = niveles[factores[index_fac]][e]   
             i += 1  
         else:
             index_row = 0
             i = 0
             index_fac += 1
-        # print(niveles[factores[index_fac]][e])
         tr_cod[index_row][index_fac] = niveles[factores[index_fac]][e]
         index_row += 1
-        #print(int(e))
-    
-    #print tr_cod
     return tr_cod
 
 def obtenerNumeroTratamientos(tratamientos):
@@ -150,53 +120,25 @@ def crearMatrixReplicas(numTratamientos,numReplicasPorTratamiento):
                                                                              # numero total de replicas
     random.shuffle(total_tests) # Barajando la lista para aleatorizar el orden de las pruebas
     matrixReplicas = total_tests.reshape(numTratamientos,numReplicasPorTratamiento) # Generando la matrix a partir de la lista
-    # print(matrixReplicas)
     return matrixReplicas
 
 
 def definirOrdenTratamientos(matrixReplicas,tratamientos):
     ordenTratamientos = []
-    print(tratamientos[0])
-    print(matrixReplicas.shape)
     for rep in range(1,matrixReplicas.shape[0]*matrixReplicas.shape[1] + 1):
         index = np.where(matrixReplicas==rep)
         rep += 1
-        #print(index)
-        #print(index[0])
-        #print(tratamientos[index[0]])
         E = tratamientos[index[0]][0].tolist()
-        #print(E)
         ordenTratamientos.append(E)
     return ordenTratamientos
         
-    """
-    for rep in range(1,matrixReplicas.shape[0]*matrixReplicas.shape[1] + 1):
-        index = np.where(matrixReplicas==rep)
-        rep += 1
-        # print(index)
-        # print(tratamientos[index[0]][0])
-        E = tratamientos[index[0]][0]
-        ordenTratamientos.append([E[0],E[1]])
-    """
 
-
-    
-
-"""
-tratamientos[:,0][tratamientos[:,0] == '0'] = niveles['controlador'][0]
-tratamientos[:,0][tratamientos[:,0] == '1'] = niveles['controlador'][1]
-tratamientos[:,1][tratamientos[:,1] == '0'] = niveles['trafico'][0]
-tratamientos[:,1][tratamientos[:,1] == '1'] = niveles['trafico'][1]
-tratamientos[:,1][tratamientos[:,1] == '2'] = niveles['trafico'][2]
-"""
 if __name__ == "__main__":
-    info("Ensayo")
+    print("Ensayo")
     niveles = { 'controlador': ['ryu','pox'],
             'trafico': ['normal','ataque1','ataque2']
     }
     tratamientos = obtenerTratamientos(niveles)
-    print(tratamientos)
-    print(type(tratamientos))
     tr_cod = codificarTratamientos(niveles)
     n_tr = obtenerNumeroTratamientos(tr_cod)
     m = crearMatrixReplicas(n_tr,20)
